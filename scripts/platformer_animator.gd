995746be1b7153f6ebc62ed302a9c0eb5ac868b0
klaswell16 <168109909+klaswell16@users.noreply.github.com>
2025-10-29 18:07:18 -0400

    Add wall jump and animation

--- scripts/platformer_animator.gd
+++ scripts/platformer_animator.gd
@@ -2,12 +2,21 @@ extends Node2D
 
 var speed = 0
 var on_ground = false
+var is_sliding = false
 @export var animated_sprite : AnimatedSprite2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if speed > .2 and on_ground:
-		animated_sprite.play("run")
+	if is_sliding:
+		animated_sprite.play("on_wall")
+		
+	elif on_ground:
+		if speed > 0.2:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("default")
+	
 	else:
-		animated_sprite.play("default")
+		# You might want separate "jump" and "fall" animations later
+		animated_sprite.play("default")  # or "jump" if you have it
--- scripts/platformer_player.gd
+++ scripts/platformer_player.gd
@@ -5,15 +5,43 @@ const JUMP_VELOCITY = -400.0
 const GRAVITY = 980.0  
 
 @onready var jump_sound = $JumpSound
+
+const WALL_JUMP_VELOCITY = Vector2(400, -450)  
+const WALL_SLIDE_SPEED = 100.0  # Max fall speed when sliding
+var max_wall_jumps = 1
+var wall_jumps_remaining = 0
+var is_wall_sliding = false
+var last_wall_normal = 0
+
 func _physics_process(delta: float) -> void:
 	# Add gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-
+	# Reset wall jump
+	if is_on_floor():
+		wall_jumps_remaining = max_wall_jumps
+		is_wall_sliding = false
+	
+	if is_on_wall() and not is_on_floor() and velocity.y > 0:
+		is_wall_sliding = true
+		velocity.y = min(velocity.y, WALL_SLIDE_SPEED)
+		last_wall_normal = get_wall_normal().x
+	else:
+		is_wall_sliding = false
+		
+	
 	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump_sound.play()
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			wall_jumps_remaining = max_wall_jumps
+			jump_sound.play()
+			velocity.y = JUMP_VELOCITY
+		elif is_wall_sliding and wall_jumps_remaining > 0:
+			velocity.x = WALL_JUMP_VELOCITY.x * -last_wall_normal
+			velocity.y = WALL_JUMP_VELOCITY.y
+			wall_jumps_remaining -= 1
+			is_wall_sliding = false
+			jump_sound.play()
 
 	# Get input direction
 	var direction := Input.get_axis("left", "right")
@@ -28,6 +56,7 @@ func _physics_process(delta: float) -> void:
 	
 	$Animator.speed = abs(velocity.x)
 	$Animator.on_ground = is_on_floor()
+	$Animator.is_sliding = is_wall_sliding
 
 	# Move and handle collisions
 	move_and_slide()
